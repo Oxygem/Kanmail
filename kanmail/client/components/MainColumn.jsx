250e586d5f22c3bfad5b87f15ceb6ef912d50e36
Nick Barrett <pointlessrambler@gmail.com>
2018-08-29 23:57:49 +0100

    Hacky: use column count + element width to make search bar the right width.

--- kanmail/client/components/MainColumn.jsx
+++ kanmail/client/components/MainColumn.jsx
@@ -15,6 +15,7 @@ export default class MainColumn extends React.Component {
         filterStore: PropTypes.object.isRequired,
         getPreviousColumn: PropTypes.func.isRequired,
         getNextColumn: PropTypes.func.isRequired,
+        columnsCount: PropTypes.number.isRequired,
     }
 
     getDecoratedComponentInstance() {
--- kanmail/client/components/Search.jsx
+++ kanmail/client/components/Search.jsx
@@ -1,19 +1,26 @@
 import _ from 'lodash';
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import keyboard from 'keyboard.js';
+
 import emailStoreProxy from 'stores/emailStoreProxy.js';
+import settingsStore from 'stores/settings.js';
+import { subscribe } from 'stores/base.jsx';
 
 
+@subscribe(settingsStore)
 export default class Search extends React.Component {
-    static propTypes = {}
+    static propTypes = {
+        columnsCount: PropTypes.number.isRequired,
+    }
 
     constructor(props) {
         super(props);
 
         this.state = {
             searchValue: null,
+            searchWidth: 100,
         };
 
         this.executeSearch = _.debounce(
@@ -22,7 +29,12 @@ export default class Search extends React.Component {
         );
     }
 
+    componentDidMount() {
+        this.calculateWidth();
+    }
+
     componentDidUpdate() {
+        this.calculateWidth();
         this.executeSearch();
     }
 
@@ -34,6 +46,32 @@ export default class Search extends React.Component {
         }
     }
 
+    calculateWidth = () => {
+        const columnElements = document.querySelectorAll('div.column');
+
+        if (!columnElements) {
+            return;
+        }
+
+        const columnsWidth = _.sum(_.map(
+            columnElements,
+            column => column.clientWidth,
+        ));
+
+        const searchWidth = _.max([
+            100,
+            columnsWidth + this.props.columnsCount - 18,
+        ]);
+
+        if (this.state.searchWidth === searchWidth) {
+            return;
+        }
+
+        this.setState({
+            searchWidth,
+        });
+    }
+
     handleInputChange = (ev) => {
         const value = ev.target.value || null;
 
@@ -43,17 +81,21 @@ export default class Search extends React.Component {
     }
 
     render() {
+
         return (
-            <div id="search">
+            <section id="search">
                 <input
                     type="text"
                     value={this.searchValue}
                     onChange={this.handleInputChange}
                     onFocus={keyboard.disable}
                     onBlur={keyboard.enable}
-                    placeholder="Search"
+                    placeholder="type to search..."
+                    style={{
+                        width: `${this.state.searchWidth}px`,
+                    }}
                 />
-            </div>
+            </section>
         );
     }
 }
