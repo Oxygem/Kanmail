4a0fb2697f7d68a56960819b575222eb746102a0
Nick Barrett <pointlessrambler@gmail.com>
2018-09-03 13:43:29 -0400

    Add pending requests to the sidebar footer.

--- kanmail/client/components/FooterStatus.jsx
+++ kanmail/client/components/FooterStatus.jsx
@@ -10,11 +10,13 @@ export default class FooterStatus extends Component {
     static propTypes = {
         fetchRequests: PropTypes.array.isRequired,
         pushRequests: PropTypes.array.isRequired,
+        pendingPushRequests: PropTypes.array.isRequired,
     }
 
     render() {
         const fetchCount = this.props.fetchRequests.length;
         const pushCount = this.props.pushRequests.length;
+        const pendingCount = this.props.pendingPushRequests.length;
 
         return (
             <div id="footer-status">
@@ -25,6 +27,10 @@ export default class FooterStatus extends Component {
                 <span className={pushCount > 0 ? 'red' : ''}>
                     <i className="fa fa-arrow-up"></i>
                     {pushCount}
+                </span>
+                <span className={pendingCount > 0 ? 'yellow' : ''}>
+                    <i className="fa fa-clock-o"></i>
+                    {pendingCount}
                 </span>
             </div>
         );
