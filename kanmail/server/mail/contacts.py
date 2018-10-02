68c4298a959a60e3fe1eb29bbc64eeffd7b42f67
Nick Barrett <pointlessrambler@gmail.com>
2018-10-02 22:04:28 +0100

    Fix the lock on the contacts cache file.

--- kanmail/server/mail/contacts.py
+++ kanmail/server/mail/contacts.py
@@ -1,10 +1,13 @@
 import pickle
 
 from os import path
+from threading import Lock
 
 from pydash import memoize
 
 from kanmail.settings import CONTACTS_CACHE_FILE
+
+CONTACTS_CACHE_LOCK = Lock()
 
 
 @memoize
@@ -12,19 +15,20 @@ def get_contacts():
     if not path.exists(CONTACTS_CACHE_FILE):
         return {}
 
-    with open(CONTACTS_CACHE_FILE, 'rb') as f:
-        return pickle.loads(f.read())
+    with CONTACTS_CACHE_LOCK:
+        with open(CONTACTS_CACHE_FILE, 'rb') as f:
+            return pickle.loads(f.read())
 
 
 def set_contacts(contacts):
     data = pickle.dumps(contacts)
-    with open(CONTACTS_CACHE_FILE, 'wb') as f:
-        f.write(data)
 
-    # Set the pydash.memoize's cache on get_contacts
-    get_contacts.cache = {
-        None: contacts,
-    }
+    with CONTACTS_CACHE_LOCK:
+        with open(CONTACTS_CACHE_FILE, 'wb') as f:
+            f.write(data)
+
+    # Reset pydash.memoize's cache for next call to get_contacts
+    get_contacts.cache = {}
 
 
 def add_contact(contact):
