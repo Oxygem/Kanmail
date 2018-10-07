9c1eff66efa2bbb15ad15315e42631bb697197f3
Nick Barrett <pointlessrambler@gmail.com>
2018-10-07 19:40:11 +0100

    Rename `boot.py` -> `main.py`.

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from time import sleep
 import webview
 
 from kanmail import settings
+from kanmail.device import register_or_ping_device, update_device
 from kanmail.log import logger
 from kanmail.server.app import app, boot
 from kanmail.window import create_window
 
 
-def run_server_thread():
+def run_server():
     try:
         boot()
         app.run(
@@ -21,7 +22,7 @@ def run_server_thread():
         )
 
     except Exception as e:
-        logger.critical('Exception in server thread!: {0}'.format(e))
+        logger.exception('Exception in server thread!: {0}'.format(e))
 
 
 def monitor_threads(*threads):
@@ -34,11 +35,20 @@ def monitor_threads(*threads):
             sleep(0.5)
 
 
+def register_and_update_device():
+    try:
+        register_or_ping_device()
+        update_device()
+    except Exception as e:
+        logger.exception('Exception in register & update thread!: {0}'.format(e))
+
+
 def main():
-    server_thread = Thread(name='Server', target=run_server_thread)
+    server_thread = Thread(name='Server', target=run_server)
     server_thread.daemon = True
     server_thread.start()
 
+    # Let's hope this thread doesn't fail!
     monitor_thread = Thread(
         name='Thread monitor',
         target=monitor_threads,
@@ -46,6 +56,15 @@ def main():
     )
     monitor_thread.daemon = True
     monitor_thread.start()
+
+    # Register & attempt update immediately - we don't kill the whole app if
+    # this fails as it's non-essential stuff.
+    register_update_thread = Thread(
+        name='Register & check for updates',
+        target=register_and_update_device,
+    )
+    register_update_thread.daemon = True
+    register_update_thread.start()
 
     # First/main call to webview, blocking - when this is quit the threads will
     # all be killed off (daemon=True).
