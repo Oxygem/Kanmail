adee50149a96a48431e36924e9d91b74c973546d
Nick Barrett <pointlessrambler@gmail.com>
2018-10-22 00:50:10 +0200

    Support `settings.style.header_background`.

--- kanmail/client/components/emails/Sidebar.jsx
+++ kanmail/client/components/emails/Sidebar.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Filters from 'components/emails/Filters.jsx';
 import SidebarHeader from 'components/emails/SidebarHeader.jsx';
 import FooterStatus from 'components/emails/FooterStatus.jsx';
 
+import settingsStore from 'stores/settings.js';
 import { get } from 'util/requests.js';
+import { subscribe } from 'stores/base.jsx';
 
 
+@subscribe(settingsStore)
 export default class Sidebar extends React.Component {
+    static propTypes = {
+        styleSettings: PropTypes.object.isRequired,
+    }
+
+    getHeaderStyles() {
+        if (this.props.styleSettings.header_background) {
+            return {
+                background: this.props.styleSettings.header_background,
+            };
+        }
+    }
     render() {
         return (
             <section id="sidebar">
-                <header>
+                <header style={this.getHeaderStyles()}>
                     <h1>
                         <span>K-</span>
                         <i className="logo fa fa-envelope-o"></i>
