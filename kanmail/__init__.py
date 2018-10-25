4505771cd55472908c0296efdec82697c21fdbdf
Nick Barrett <pointlessrambler@gmail.com>
2018-10-25 18:51:06 +0100

    Default loggers to WARN.

--- kanmail/__init__.py
+++ kanmail/__init__.py
@@ -1,5 +1,10 @@
+import logging
 import sys
 
 
 if sys.version_info.major != 3:
     raise RuntimeError('Python 2 is *not* supported.')
+
+
+# Set the default/global log level to WARN
+logging.getLogger().setLevel(logging.WARNING)
