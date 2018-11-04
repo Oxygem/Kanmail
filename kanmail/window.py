9b7a65192ce76e05045b5e7792ea0c61f8b3077e
Nick Barrett <pointlessrambler@gmail.com>
2018-11-04 13:22:05 +0000

    Debug log when opening windows.

--- kanmail/window.py
+++ kanmail/window.py
@@ -1,10 +1,13 @@
 import webview
 
+from kanmail.log import logger
 from kanmail.settings import DEBUG, SERVER_PORT
 
 
 def create_window(title='Kanmail', endpoint='/', **kwargs):
     link = f'http://localhost:{SERVER_PORT}{endpoint}'
+
+    logger.debug(f'Opening window {title}: url={endpoint} kwargs={kwargs}')
 
     return webview.create_window(
         title, link,
