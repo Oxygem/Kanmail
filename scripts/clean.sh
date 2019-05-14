4c7d61293240a5dace13840be7bb43d11ec72a75
Nick Barrett <pointlessrambler@gmail.com>
2019-05-14 10:45:19 +0100

    Extend clean script to remove any incomplete builds/changelog.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+set -ex
+
 echo "--> nuking dist/ & build/"
 rm -rf dist/
 rm -rf build/
+
+echo "--> nuking anything in pyu-data/new"
+rm -rf pyu-data/new/*
+
+echo "--> nuking CHANGELOG.md changes"
+git checkout -- CHANGELOG.md
+
