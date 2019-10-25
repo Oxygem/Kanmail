a8f3e973d63cd9ca39bb44e41d166052710ae0f7
Nick Barrett <pointlessrambler@gmail.com>
2019-10-25 14:53:47 +0100

    Doesn't work because the webserver cannot open windows, ugh!
    Revert "Implement full dev reloading."
    
    This reverts commit 1dc22666df44e6ba72bf6a23fdf9f2ae4d86d48b.

--- main.py
+++ main.py
@@ -57,19 +57,18 @@ def run_thread(target):
 def main():
     logger.info(f'\n#\n# Booting Kanmail {get_version()}\n#')
 
-    if '--no-server' not in sys.argv:
-        server_thread = Thread(name='Server', target=run_server)
-        server_thread.daemon = True
-        server_thread.start()
+    server_thread = Thread(name='Server', target=run_server)
+    server_thread.daemon = True
+    server_thread.start()
 
-        # Let's hope this thread doesn't fail!
-        monitor_thread = Thread(
-            name='Thread monitor',
-            target=monitor_threads,
-            args=(server_thread,),
-        )
-        monitor_thread.daemon = True
-        monitor_thread.start()
+    # Let's hope this thread doesn't fail!
+    monitor_thread = Thread(
+        name='Thread monitor',
+        target=monitor_threads,
+        args=(server_thread,),
+    )
+    monitor_thread.daemon = True
+    monitor_thread.start()
 
     # Register/ping immediately - without caring if it fails
     run_thread(register_or_ping_device)
