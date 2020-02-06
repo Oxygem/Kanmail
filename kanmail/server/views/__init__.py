ee6cae2ee0ffce847fc2818590115a22eced416e
Nick Barrett <pointlessrambler@gmail.com>
2020-02-06 10:56:04 +0100

    Pass frameless setting to the frontend.

--- kanmail/server/views/__init__.py
+++ kanmail/server/views/__init__.py
@@ -9,7 +9,7 @@ from kanmail.log import logger
 from kanmail.server.app import app
 from kanmail.server.mail.contacts import get_contacts
 from kanmail.server.util import get_or_400
-from kanmail.settings import DEBUG, FROZEN, IS_APP
+from kanmail.settings import DEBUG, FRAMELESS, FROZEN, IS_APP
 from kanmail.version import get_version
 from kanmail.window import create_open_dialog, create_window, destroy_window
 
@@ -28,6 +28,7 @@ def _get_render_data():
         'frozen': FROZEN,
         'debug': DEBUG,
         'is_app': IS_APP,
+        'frameless': FRAMELESS,
     }
 
 
