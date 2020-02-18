052818139826280ea257027cc856284698c8c5d1
Nick Barrett <pointlessrambler@gmail.com>
2020-02-18 10:32:22 +0000

    Make `HeaderBar` reflect the header background style setting.

--- kanmail/client/components/HeaderBar.jsx
+++ kanmail/client/components/HeaderBar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import settingsStore from 'stores/settings.js';
+import { subscribe } from 'stores/base.jsx';
 
 
+@subscribe(settingsStore)
 export default class HeaderBar extends React.Component {
+    static propTypes = {
+        styleSettings: PropTypes.object.isRequired,
+    }
+
+    getHeaderStyles() {
+        if (this.props.styleSettings.header_background) {
+            return {
+                background: this.props.styleSettings.header_background,
+            };
+        }
+    }
+
     render() {
         return (
-            <header>
+            <header style={this.getHeaderStyles()}>
                 <h1 id="logo">
                     <span>K-</span>
                     <i className="logo fa fa-envelope-o"></i>
