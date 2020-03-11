a92de2bdff4f21f16d1eda0ff129200a32c9c4f0
Nick Barrett <pointlessrambler@gmail.com>
2020-03-11 09:16:15 +0000

    Make it possible to subscribe to individual store props.

--- kanmail/client/stores/base.jsx
+++ kanmail/client/stores/base.jsx
@@ -4,6 +4,17 @@ import React from 'react';
 import { lowercaseFirstLetter } from 'util/string.js';
 
 
+const getStorePropNames = store => {
+    let propNames = _.keys(store.props);
+
+    if (_.isArray(store)) {
+        [store, propNames] = store;
+    }
+
+    return [store, propNames];
+}
+
+
 export function subscribe(...stores) {
     return Component => class Connect extends React.Component {
         constructor(props) {
@@ -12,27 +23,29 @@ export function subscribe(...stores) {
             const state = {};
 
             // Attach the store with it's name
-            _.each(stores, store => {
+            _.each(stores, storeConfig => {
+                const [store, propNames] = getStorePropNames(storeConfig);
+
                 state[lowercaseFirstLetter(store.constructor.storeKey)] = store;
 
                 // Extend by the store's provided properties
-                _.extend(state, store.props);
+                _.extend(state, _.pick(store.props, propNames));
             });
 
             this.state = state;
         }
 
         componentDidMount() {
-            _.each(stores, store => {
-                // Subscribe to store changes
-                store.subscribe(this);
+            _.each(stores, storeConfig => {
+                const [store, propNames] = getStorePropNames(storeConfig);
+                store.subscribe(this, propNames);
             });
         }
 
         componentWillUnmount() {
-            _.each(stores, store => {
-                // Unsubscribe to store changes
-                store.unsubscribe(this);
+            _.each(stores, storeConfig => {
+                const [store, propNames] = getStorePropNames(storeConfig);
+                store.unsubscribe(this, propNames);
             });
         }
 
@@ -55,18 +68,26 @@ export class BaseStore {
         this.props = {};
     }
 
-    subscribe(app) {
-        this.apps.push(app);
+    subscribe(app, propNames) {
+        this.apps.push([app, propNames]);
     }
 
-    unsubscribe(app) {
-        this.apps = _.without(this.apps, app);
+    unsubscribe(app, propNames) {
+        this.apps = _.without(this.apps, [app, propNames]);
     }
 
-    triggerUpdate() {
+    triggerUpdate(updatedPropNames) {
+        if (!updatedPropNames) {
+            updatedPropNames = _.keys(this.props);
+        }
+
         // For each wrapped app, set it's state with the stores props
         _.each(this.apps, app => {
-            app.setState(this.props);
+            const [component, propNames] = app;
+            const intersection = _.intersection(updatedPropNames, propNames);
+            if (intersection.length > 0) {
+                component.setState(_.pick(this.props, propNames));
+            }
         });
     }
 }
