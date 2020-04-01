4500fb6e2d2d159d261d31f5306fe41ffe3c4451
Nick Barrett <pointlessrambler@gmail.com>
2020-04-01 09:56:54 +0100

    First pass implementation at MacOS notarization during build.

--- make/macos.py
+++ make/macos.py
@@ -1,8 +1,9 @@
-from os import path, symlink
+from os import path, symlink, unlink
 from shutil import rmtree
+from time import sleep
 
-from .settings import CODESIGN_KEY_NAME
-from .util import print_and_run
+from .settings import CODESIGN_KEY_NAME, NOTARIZE_PASSWORD_KEYCHAIN_NAME
+from .util import print_and_check_output, print_and_run
 
 
 def codesign(app_dir):
@@ -32,9 +33,54 @@ def codesign(app_dir):
         '--deep',
         '--timestamp',
         '--options', 'runtime',
-        '-s', CODESIGN_KEY_NAME,
+        '--sign', CODESIGN_KEY_NAME,
         app_dir,
     ))
+    print_and_run(('codesign', '--verify', app_dir))
+
+
+def wait_for_notarization(notarize_request_id):
+    while True:
+        notarize_status = print_and_check_output((
+            'xcrun',
+            'altool',
+            '--password', f'@keychain:{NOTARIZE_PASSWORD_KEYCHAIN_NAME}',
+            '--notarization-info', notarize_request_id,
+        ))
+
+        if 'Status: in progress' not in notarize_status:
+            break
+
+        sleep(5)
+
+    return 'Status: success' in notarize_status
+
+
+def notarize(version, app_dir, zip_filename):
+    print_and_run(('ditto', '-c', '-k', '--keepParent', app_dir, zip_filename))
+
+    try:
+        notarize_response = print_and_check_output((
+            'xcrun',
+            'altool',
+            '--notarize-app',
+            '--primary-bundle-id', version,
+            '--password', f'@keychain:{NOTARIZE_PASSWORD_KEYCHAIN_NAME}',
+            '--file', zip_filename,
+        ))
+
+        for line in notarize_response.splitlines():
+            if line.startswith('RequestUUID'):
+                notarize_request_id = line.split('=')[1].strip()
+
+        did_succeed = wait_for_notarization(notarize_request_id)
+        if not did_succeed:
+            raise Exception(f'Failed to notarize app, requestID={notarize_request_id}')
+
+        print_and_run(('xcrun', 'stapler', 'staple', app_dir))
+        print_and_run(('xcrun', 'stapler', 'validate', app_dir))
+    finally:
+        unlink(zip_filename)
 
 
 def codesign_and_notarize(version):
@@ -43,9 +89,11 @@ def codesign_and_notarize(version):
     filename = path.join(new_builds_dir, f'Kanmail-mac-{version}.tar.gz')
     print_and_run(('gtar', '-C', new_builds_dir, '-xzf', filename))
     app_name = 'Kanmail.app'
+    zip_filename = path.join(new_builds_dir, f'{app_name}.zip')
     app_dir = path.join(new_builds_dir, app_name)
 
     codesign(app_dir)
+    notarize(version, app_dir, zip_filename)
 
     print_and_run(('gtar', '-C', new_builds_dir, '-zcf', filename, app_name))
 
