2043e93c82b078caafac19dba8acd7fb077a1913
Nick Barrett <nick@oxygem.com>
2020-09-02 09:41:58 +0100

    Icon support for iCloud/Outlook accounts.

--- kanmail/client/util/accounts.js
+++ kanmail/client/util/accounts.js
@@ -2,5 +2,14 @@ export function getAccountIconName(account) {
     if (account.imap_connection.host === 'imap.gmail.com') {
         return 'google';
     }
+
+    if (account.imap_connection.host === 'imap.mail.me.com') {
+        return 'apple';
+    }
+
+    if (account.imap_connection.host === 'imap-mail.outlook.com') {
+        return 'windows';
+    }
+
     return 'envelope';
 }
