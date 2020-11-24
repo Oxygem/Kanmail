3b7f5acf4c6daccf604db92c46fabd1adf56a497
Nick Barrett <nick@oxygem.com>
2020-11-24 09:50:24 +0000

    Make keyboard column scrolling smoother & consistent.
    Notably this does not work on MacOS/Safari yet.

--- kanmail/client/keyboard.js
+++ kanmail/client/keyboard.js
@@ -4,6 +4,7 @@ import threadStore from 'stores/thread.js';
 import requestStore from 'stores/request.js';
 import controlStore from 'stores/control.js';
 
+import { ensureInView } from 'util/element.js';
 import {
     getNextThreadComponent,
     getPreviousThreadComponent,
@@ -31,25 +32,6 @@ const keys = {
     ARROW_DOWN: 40,
 };
 const validKeyCodes = _.values(keys);
-
-
-function isInViewport(element) {
-    var rect = element.getBoundingClientRect();
-    var html = document.documentElement;
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || html.clientHeight) &&
-        rect.right <= (window.innerWidth || html.clientWidth)
-    );
-}
-
-
-function ensureInView(element, alignToTop) {
-    if (!isInViewport(element)) {
-        element.scrollIntoView(alignToTop);
-    }
-}
 
 
 class Keyboard {
@@ -91,12 +73,15 @@ class Keyboard {
         }
     }
 
-    selectThread = (thread) => {
+    selectThread = (thread, scrollToBlockOption) => {
         if (thread) {
             this.setThreadComponent(thread);
 
             if (thread.element) {
-                ensureInView(thread.element, false);
+                ensureInView(thread.element, {
+                    behavior: 'smooth',
+                    block: scrollToBlockOption,
+                });
             }
 
             if (threadStore.isOpen) {
@@ -109,12 +94,12 @@ class Keyboard {
 
     selectNextThread = () => {
         const nextThread = getNextThreadComponent(this.currentComponent);
-        return this.selectThread(nextThread);
+        return this.selectThread(nextThread, 'end');
     }
 
     selectPreviousThread = () => {
         const previousThread = getPreviousThreadComponent(this.currentComponent);
-        return this.selectThread(previousThread);
+        return this.selectThread(previousThread, 'start');
     }
 
     selectNextColumnThread = () => {
