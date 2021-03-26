0d989cd0646006d5f79c88a4a40c5191a77aebaa
Nick Barrett <nick@oxygem.com>
2021-03-26 16:57:38 +0000

    Improve style of the error boundary.

--- kanmail/client/components/ErrorBoundary.jsx
+++ kanmail/client/components/ErrorBoundary.jsx
@@ -31,10 +31,10 @@ export default class ErrorBoundary extends React.Component {
         }
 
         return (
-            <div>
-                <h1>Something broke!</h1>
+            <div id="no-app">
+                <h1><img src="/favicon.ico" /> Something broke!</h1>
                 <p><a onClick={() => window.location.reload()}>Click here to reload!</a></p>
-                <p>So this is embarrassing - something broke!</p>
+                <p>So this is embarrassing - something broke! If this error persists, please go to: <a target="_blank" rel="noreferrer" href="https://kanmail.io/support">kanmail.io/support</a>.</p>
                 <pre><code>{this.state.errorInfo.componentStack}</code></pre>
             </div>
         );
