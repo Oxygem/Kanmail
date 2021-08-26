e637a472d0ce30ee9611f584107f167940cfac18
Nick Barrett <nick@oxygem.com>
2021-08-26 11:37:01 +0100

    Fixes for building Linux client in Docker.

--- make/build_linux_client_docker.sh
+++ make/build_linux_client_docker.sh
@@ -10,5 +10,9 @@ docker build \
 
 docker run \
     -v `pwd`:/opt/kanmail \
+    -e SENTRY_DSN=$SENTRY_DSN \
+    -e POSTHOG_API_KEY=$POSTHOG_API_KEY \
+    -e GOOGLE_OAUTH_CLIENT_ID=$GOOGLE_OAUTH_CLIENT_ID \
+    -e GOOGLE_OAUTH_CLIENT_SECRET=$GOOGLE_OAUTH_CLIENT_SECRET \
     kanmail-ubuntu-linux-build \
     python -m make $@
