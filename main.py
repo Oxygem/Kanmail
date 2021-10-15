6c1526470949a230e8568530c23f8f135cc978ca
Nick Barrett <nick@oxygem.com>
2021-10-15 17:21:30 +0100

    Improve exit handling in main script.

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ def monitor_threads(*threads):
             if not thread.is_alive():
                 logger.critical(f'Thread: {thread} died, exiting!')
                 destroy_main_window()
+                server.stop()
+                sys.exit(2)
         else:
             sleep(0.5)
 
@@ -110,9 +112,14 @@ def main():
     # Start the GUI - this will block until the main window is destroyed
     webview.start(gui=GUI_LIB, debug=DEBUG)
 
-    # Main window closed, cleanup/exit
+    logger.debug('Main window closed, shutting down...')
+    server.stop()
     sys.exit()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception:
+        server.stop()
+        raise
