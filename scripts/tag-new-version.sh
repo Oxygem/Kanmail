60b2703631173dd4e2e1f7a12e0d267d1c3eb758
Nick Mills-Barrett <nick@oxygem.com>
2025-10-17 22:27:56 +0100

    Improve tag script

--- scripts/tag-new-version.sh
+++ scripts/tag-new-version.sh
@@ -10,4 +10,4 @@ fi
 export KANMAIL_APP_VERSION="2.$(date '+%y%m%d%H%M%S')"
 
 git tag -a $KANMAIL_APP_VERSION -m $KANMAIL_APP_VERSION
-git push --tags
+git push --atomic origin 2.x $KANMAIL_APP_VERSION
