a5e6dc4f6ccdfef26c8692d2991970ca3c24bf66
Nick Mills-Barrett <nick@oxygem.com>
2025-11-20 21:55:37 +0000

    Restart the entire app after crash display

--- frontend/src/components/ErrorInformation.tsx
+++ frontend/src/components/ErrorInformation.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { AppService } from "../../bindings/github.com/oxygem/kanmail/internal/services/index.ts";
+
 interface IErrorInformationProps {
   error: any;
   componentStack: any;
@@ -22,7 +24,7 @@ class ErrorInformation extends React.Component<IErrorInformationProps, IErrorInf
           <img src="/icon.png" width="24px" /> Something broke!
         </h1>
         <p>
-          <a onClick={() => window.location.reload()}>Click here to reload!</a>
+          <a onClick={() => AppService.RestartAfterUpdate()}>Click here to reload!</a>
         </p>
         <p>
           So this is embarrassing - something broke! If this error persists,
--- frontend/src/components/HeaderErrors.tsx
+++ frontend/src/components/HeaderErrors.tsx
@@ -1,5 +1,4 @@
 import _ from "lodash";
-import PropTypes from "prop-types";
 import React, { Component } from "react";
 
 import { SUPPORT_DOC_LINK } from "../constants.ts";
