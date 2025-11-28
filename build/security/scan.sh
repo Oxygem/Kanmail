ab88406fb846cc768f4649f408d20269625bb268
Nick Mills-Barrett <nick@oxygem.com>
2025-11-28 11:35:32 +0000

    Add security scanning to CI

---
+++ build/security/scan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [ -z "${1:-}" ] || [ "${1}" = "sast" ]; then
+    echo "Performing SAST scan..."
+    docker build -t kanmail-scan-sast -f ./build/security/Dockerfile-sast .
+    docker run kanmail-scan-sast sast scan /opt/kanmail/build/security/scanner.yaml
+fi
+
+if [ -z "${1:-}" ] || [ "${1}" = "sca" ]; then
+    echo "Performing SCA scan..."
+    docker build -t kanmail-scan-sca -f ./build/security/Dockerfile-sca .
+    docker run kanmail-scan-sca sca scan /opt/kanmail/build/security/scanner.yaml
+fi
