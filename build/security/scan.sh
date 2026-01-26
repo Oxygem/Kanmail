fe1658d7a658a5dd92ae74517cd10dad9c28e34d
Nick Mills-Barrett <nick@oxygem.com>
2026-01-26 10:45:58 +0000

    Don't fail CI for SCA scan results
    Currently impossible to pass because there's a nested dependency (unused)
    that has a low risk CVE attached and the fluidattacks config documentation
    either is out of date or is now broken.

--- build/security/scan.sh
+++ build/security/scan.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 if [ -z "${1:-}" ] || [ "${1}" = "sast" ]; then
     echo "Performing SAST scan..."
     docker build -t kanmail-scan-sast -f ./build/security/Dockerfile-sast .
-    docker run kanmail-scan-sast sast scan /opt/kanmail/build/security/scanner.yaml
+    docker run kanmail-scan-sast sast --strict scan /opt/kanmail/build/security/scanner.yaml
 fi
 
 if [ -z "${1:-}" ] || [ "${1}" = "sca" ]; then
