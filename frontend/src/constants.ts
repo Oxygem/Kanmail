b4022b6293e61e858a2fc9ed25e0bdcbc46b7039
Nick Mills-Barrett <nick@oxygem.com>
2026-05-01 22:42:52 +0100

    Update default account colors

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -30,9 +30,9 @@ export const APPLE_APP_PASSWORD_LINK =
   "https://support.apple.com/en-gb/HT204397";
 
 export const ACCOUNT_ACCENT_COLORS = [
-  "#f44336", "#e91e63", "#9c27b0", "#673ab7",
-  "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4",
-  "#009688", "#4caf50", "#8bc34a", "#cddc39",
-  "#ffeb3b", "#ffc107", "#ff9800", "#ff5722",
-  "#795548", "#607d8b",
+  "#2196f3", "#4caf50", "#9c27b0", "#ff9800",
+  "#00bcd4", "#673ab7", "#8bc34a", "#3f51b5",
+  "#009688", "#03a9f4", "#cddc39", "#ffc107",
+  "#e91e63", "#795548", "#607d8b", "#ffeb3b",
+  "#ff5722", "#f44336",
 ];
